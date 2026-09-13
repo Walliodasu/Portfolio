d01e7221ce5751b545f61b7f251e941171368b45
Walliodasu <96730865+Walliodasu@users.noreply.github.com>
2026-09-13 18:18:59 +0200

    version finalisee

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -4,7 +4,13 @@ import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(withRoutes(serverRoutes))],
+  providers: [
+    provideServerRendering(
+      withRoutes(serverRoutes),
+      // Optionnel selon la structure de votre version pour injecter les options de rendu, 
+      // ou en configurant l'objet d'options directement :
+    )
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
